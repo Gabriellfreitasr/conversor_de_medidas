c0342b710ff8990774c8f3784a45028a44a6b872
Gabriellfreitasr <GabriellFreitas456@gmail.com>
2022-04-15 13:59:31 -0300

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -1,15 +1,15 @@
 #Conversão de medida
-print("\t ----Conversão de medida----")
+print (" \t ----Conversão de medida----")
 
 opçao = int(input("Digite 1 para converter Metros para centimetros: \nOu 2 para converter Centimetros para metros: "))
 
-if opçao == 1:
-    metros = int(input("Digite o valor em metros: "))
-    print("O valor de {} metros, em centimetros é:".format(metros), metros*100)
+if opçao ==  1:
+    metros  =  int(input("Digite o valor em metros: "))
+    print("O valor de {} metros, em centímetros é:".format(metros), metros*100)
 
-elif opçao == 2:
-    centimetros = int(input("Digite o valor em Centimetros: "))
+elif opçao ==  2:
+    centimetros  =  int(input("Digite o valor em Centimetros: "))
     print("O valor de {} centimetros, em metros é de:".format(centimetros), centimetros/100)
 
 else:
-    print("Valor não identificado! ")
+    print("Valor não identificado!")
